b51195827f579608b1841ccc7419dd888ee99f98
destineevaldivia <destvaldivia@gmail.com>
2023-09-07 14:52:19 -0700

    both servers running

--- server/index.js
+++ server/index.js
@@ -7,9 +7,20 @@ const app = express();
 //enable CORS middleware
 app.use(cors())
 //Set the port that you want the server to run on
-const PORT = 8081;
+const PORT = 5005;
 
-//create routes
+//create root route endpoint
 app.get('/', (req,res) => {
+    res.json({message: 'This is my roooooot route!'})
+});
+
+//create endpoint for /api
+app.get('/api', (req,res) => {
     res.json({message: 'Hola, Destino!'})
 });
+
+
+// get server up and running
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
