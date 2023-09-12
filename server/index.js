d3e18d4d41fcbac5e6ea88c3bef2c4e1cc4499e7
destineevaldivia <destvaldivia@gmail.com>
2023-09-12 13:06:49 -0700

    added comments to index.js on server side

--- server/index.js
+++ server/index.js
@@ -9,18 +9,22 @@ app.use(cors())
 //Set the port that you want the server to run on
 const PORT = 5005;
 
-//root route
+// setting up my root route 
 app.get('/', (req,res) => {
     res.json({message: 'This is my roooooot route! Go here--->  http://localhost:5173/'})
 });
 
-//GET route definition for /api path from external API
+//Defines the route in my app that listens for incoming GET request at /api endpoint
 app.get('/api', (req,res) => {
     const url = 'https://opentdb.com/api.php?amount=10&category=28&difficulty=easy&type=multiple'
     //use fetch to make an HTTP GET request to the specified URL
     fetch(url)
+    //promise chain that waits for the HTTP response to be received.
+    //once received, it extracts the data from the response using .json() method
         .then((response) => response.json())
+    //sends the fetched data as a JSON response back to the client that made the GET request to /api.
         .then((data) => res.json(data))
+    //handles errors
         .catch((err) => {  
             console.error(err);
             res.status(500).json({ error: 'An error occurred' });
@@ -28,7 +32,7 @@ app.get('/api', (req,res) => {
 });
 
 
-//server up and running
+//listen on PORT 5005, start server up and running
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
